b0b96ea15c971c2f4826e28dbd9f13e8d2a6cd50
arujanalpamys1607 <arujanalpamys1607@MacBook-Air-arujanalpamys1607.local>
2026-02-02 19:25:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".overlay");
+
+burger.addEventListener("click", function () {
+  menu.style.display = "block";
+});
+
+document
+  .querySelector(".courses-section")
+  .addEventListener("click", function () {
+    menu.style.display = "none";
+  });
+document.querySelector(".hero").addEventListener("click", function () {
+  menu.style.display = "none";
+});
+
+const navItems = document.querySelectorAll(".nav-item");
+const dropdown = document.querySelector(".dropdown");
+
+navItems.forEach((item) => {
+  item.addEventListener("mouseenter", function () {
+    dropdown.style.display = "flex";
+  });
+});
+document.querySelector(".nav").addEventListener("mouseleave", () => {
+  dropdown.style.display = "none";
+});
